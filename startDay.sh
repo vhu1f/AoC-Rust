4c0f2742b9684e959b6982f3095cae3f9a0912b7
vhu1f <emma.lee.latuszek@gmail.com>
2020-12-06 03:50:36 -0600

    uhohhhhh

--- startDay.sh
+++ startDay.sh
@@ -9,5 +9,6 @@ fi
 mkdir day$1
 # maybe do some template copying here? (once i know how rust works hehe)
 echo -e "# Day $1\n\n## Lessons Learned\n\n## Prompt\n\n" >> day$1/README.md
+# i should  probably put this nearly infinite session identifier into a .gitignore'd file eh? :)
 curl -b 'session=53616c7465645f5f0790246363e6722ea5c33071a70cafa28ee89df071a2f174acdafdc6759a0f2d083e4bddeaf26226' https://adventofcode.com/2020/day/$1/input >> day$1/input.txt
 cp template.rs day$1/day$1.rs
